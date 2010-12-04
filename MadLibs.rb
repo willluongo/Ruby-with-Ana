70bb91e21a52b7f0bcca4b08648fc807846c0eab
Ana <ana@studio.offthatop.com>
2010-12-03 20:24:09 -0500

    Added Ana's solo effort: Madlibs 	new file:   MadLibs.rb 	new file:   MadLibs.txt

---
+++ MadLibs.rb
@@ -0,0 +1,32 @@
+# First we prompt the player for a verb.
+puts "Pick any verb."
+first_string = gets.chomp
+# Next we prompt the player for a place.
+puts "Pick any place."
+second_string = gets.chomp
+# Afterwards we prompt player for a name.
+puts "Choose any name"
+third_string = gets.chomp
+# Next we prompt the player for a noun.
+puts "Choose a noun."
+fourth_string = gets.chomp
+# Then we prompt the player for a body part. An arm, leg, head, etc.
+puts "Chose any part of body. An arm, leg, head, etc."
+fifth_string = gets.chomp
+# After that we prompt the player for an adjective.
+puts "Pick an adjective"
+sixth_string = gets.chomp
+# Next we prompt player for another place.
+puts "Pick another place."
+seventh_string = gets.chomp
+# Then we prompt player for any game title.
+puts "Choose any random game title."
+eighth_string = gets.chomp
+# Finally we prompt player for any time of day.
+puts "Pick any time of day"
+ninth_string = gets.chomp
+# When we are done asking all questions we through all the player answers together and make it a short story.
+puts "One time while I was #{first_string} to #{second_string}, I saw my friend #{third_string}. S/he was holding a #{fourth_string} in their #{fifth_string}.It was so #{sixth_string}! We ran to #{seventh_string} and played #{eighth_string} until #{ninth_string}."
+
+
+
