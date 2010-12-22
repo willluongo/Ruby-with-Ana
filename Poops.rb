98173cbdfc01f75544d67fdff7f009809d32acf2
Ana <ana@studio.offthatop.com>
2010-12-21 21:36:06 -0500

    Worked more on assignment 	modified:   Poops.rb

--- Poops.rb
+++ Poops.rb
@@ -17,3 +17,30 @@ def total
 end
 
 #check to see if this roll wins or loses, tell the player score.
+first_roll = total
+if total == 7
+puts "you win!"
+end
+#Roll two six sided dice. 
+puts"Please type in 'roll' when you are ready.Don't forget to have fun!"
+if first_roll == 7 or 11
+puts "You Win!"
+elsif first_roll == 2 or 3 or 12
+puts "You just got POOPED OUT!Sorry you lose."
+elsif first_roll == 4 or 5 or 6 or 8 or 9 or 10
+puts "Keep on rolling. You're doing great!"
+end
+
+#Record total of both die.
+puts "@die_1.roll + @die_2.roll"
+#Figure out if you have won, lost, or need to keep rolling.
+
+
+#If you got 7 or 11 you won. If you lost you would have gotten 2,3, or 12.
+
+
+#If you got none of those numbers roll again to try and get the total you first got.
+
+
+#If you get the same number you did your first roll you win, but if you get a seven any roll but your first roll then you lose.
+
