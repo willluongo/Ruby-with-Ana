ff13d8a286e0cb9f4543ae0dfca9b9413e46ff48
Will Luongo <me@willluongo.com>
2011-10-19 20:35:36 -0400

    Added a thread to make the timing more accurate

--- TimedMathGame.rb
+++ TimedMathGame.rb
@@ -13,24 +13,35 @@ class Timer
 end
     
 if __FILE__ == $0
+  time_limit = 60.0
   correct = 0
   incorrect = 0
   timer = Timer.new
-  while timer.passed < 60.0
-    puts "\n\n\nYou have #{(60 - timer.passed).round} seconds left."
+  interrupt = false
+  
+  Thread.new {
+    while true
+      if timer.passed >= time_limit
+        puts "\nSorry, you've run out of time."
+        puts "You got #{correct} correct, and #{incorrect} incorrect."
+        puts "Your final grade is #{(((correct+incorrect).to_f/correct)*100).round}%"
+        exit
+      end
+    end
+  }
+  while timer.passed < time_limit
+    puts "\n\n\nYou have #{(time_limit - timer.passed).round} seconds left."
     num1 = rand(50)
     num2 = rand(50)
     puts "What is #{num1} + #{num2}?"
     input = gets.chomp.to_i
-    if (input == num1 + num2) and timer.passed < 60
+    if input == num1 + num2
       correct += 1
       puts "#{input} is correct!"
-    elsif timer.passed < 60
+    else
       incorrect += 1
       puts "I'm sorry, the correct answer was #{num1 + num2}."
-    else
-      puts "I'm sorry, you ran out of time before you answered, so your last answer didn't count."
     end
   end
-  puts "You have #{correct} correct, and #{incorrect} incorrect."
+  
 end
