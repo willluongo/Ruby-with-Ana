7cbc7035cc22ca9cf5cc69461f6570627b4d89c1
Will Luongo <me@willluongo.com>
2011-11-27 10:57:15 -0500

    Added more math problems, fixed problem with threads again.
    	modified:   TimedMathGame.rb

--- TimedMathGame.rb
+++ TimedMathGame.rb
@@ -21,6 +21,16 @@ def sub_gen(num1, num2)
   puts "What is #{num1} - #{num2}?"
   return num1 - num2
 end
+
+def div_gen(num1, num2)
+	puts "What is #{num1 * num2} divided by #{num2}?"
+	return num1
+end
+
+def mult_gen(num1, num2)
+	puts "What is #{num1} times #{num2}?"
+	return num1 * num2
+end
     
 if __FILE__ == $0
   Thread.abort_on_exception = true
@@ -50,13 +60,13 @@ if __FILE__ == $0
   Thread.new {
     while true
       if timer.passed >= time_limit
-        interrupt = true
         puts "\nSorry, you've run out of time."
         puts "You got #{correct} correct, and #{incorrect} incorrect."
         final_grade = ((correct/(correct+incorrect).to_f)*100).round
         puts "Your final grade is #{final_grade}%"
         file.puts("#{right_now.strftime(fmt='%T')}  #{player_name.downcase} got #{correct} out of #{correct + incorrect}, for a grade of #{final_grade}%.")
         file.close
+        interrupt = true
         exit
       end
     end
@@ -66,10 +76,17 @@ if __FILE__ == $0
     puts "\n\n\nYou have #{(time_limit - timer.passed).round} seconds left."
     num1 = rand(number_limit)
     num2 = rand(number_limit)
-    if rand(2) == 1
+    case rand(4)
+		when 0
       answer = add_gen(num1, num2)
-    else
+    when 1
       answer = sub_gen(num1, num2)
+    when 2
+  	  answer = mult_gen(num1, num2)
+	  when 3
+	  	answer = div_gen(num1, num2)
+  	else
+  		puts "WAT."
     end
     input = gets.chomp.to_i
     if input == answer
