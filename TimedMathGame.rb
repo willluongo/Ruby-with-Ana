96ec2c6e0a96faba0f3d305f6ee875b2c004bd34
Will Luongo <me@willluongo.com>
2012-01-15 15:06:52 -0500

    Moved to more object oriented approach, added redo 	modified:   TimedMathGame.rb

--- TimedMathGame.rb
+++ TimedMathGame.rb
@@ -12,25 +12,47 @@ class Timer
   end
 end
 
-def add_gen(num1, num2)
-  puts "What is #{num1} + #{num2}?"
-  return num1 + num2
-end
-
-def sub_gen(num1, num2)
-  puts "What is #{num1} - #{num2}?"
-  return num1 - num2
-end
-
-def div_gen(num1, num2)
-	num2 = num2 + 1
-	puts "What is #{num1 * num2} divided by #{num2}?"
-	return num1
-end
-
-def mult_gen(num1, num2)
-	puts "What is #{num1} times #{num2}?"
-	return num1 * num2
+class Problem
+  def initialize(max_value)
+    @type = rand(4)
+    @num_1 = rand(max_value)
+    @num_2 = rand(max_value)
+    case @type
+    when 0
+      @operator = "plus"
+    when 1
+      @operator = "minus"
+    when 2
+      @operator = "divided by"
+    when 3
+      @operator = "times"
+    else
+      abort("Illegal problem type.")
+    end
+  end
+  
+  def prompt
+    unless @type == 2
+      puts "What is #{@num_1} #{@operator} #{@num_2}?"
+    else
+      puts "What is #{@num_1 * (@num_2 + 1)} divided by #{@num_2 + 1}?"
+    end
+  end
+  
+  def answer
+    case @type
+    when 0
+      return @num_1 + @num_2
+    when 1
+      return @num_1 - @num_2
+    when 2
+      return @num_1
+    when 3
+      return @num_1 * @num_2
+    else
+      abort("Illegal problem type.")
+    end
+  end
 end
     
 if __FILE__ == $0
@@ -39,6 +61,7 @@ if __FILE__ == $0
   player_name = gets.chomp
   
   right_now = DateTime.now
+  @redos = []
   this_day = right_now.strftime(fmt='%Y%m%d')
   player_file = "./#{player_name.downcase}-#{this_day}.txt"
   
@@ -61,42 +84,54 @@ if __FILE__ == $0
   Thread.new {
     while true
       if timer.passed >= time_limit
-        puts "\nSorry, you've run out of time."
-        puts "You got #{correct} correct, and #{incorrect} incorrect."
-        final_grade = ((correct/(correct+incorrect).to_f)*100).round
-        puts "Your final grade is #{final_grade}%"
-        file.puts("#{right_now.strftime(fmt='%T')}  #{player_name.downcase} got #{correct} out of #{correct + incorrect}, for a grade of #{final_grade}%.")
-        file.close
         interrupt = true
-        exit
+        sleep 1
+      else
+        sleep 1
       end
     end
   }
   
   until interrupt
     puts "\n\n\nYou have #{(time_limit - timer.passed).round} seconds left."
-    num1 = rand(number_limit)
-    num2 = rand(number_limit)
-    case rand(4)
-		when 0
-      answer = add_gen(num1, num2)
-    when 1
-      answer = sub_gen(num1, num2)
-    when 2
-  	  answer = mult_gen(num1, num2)
-	  when 3
-	  	answer = div_gen(num1, num2)
-  	else
-  		puts "WAT."
-    end
+    problem = Problem.new(number_limit)
+    problem.prompt
     input = gets.chomp.to_i
-    if input == answer
-      correct += 1
-      puts "#{input} is correct!"
-      number_limit += 2
+    unless interrupt
+      if input == problem.answer
+        correct += 1
+        puts "#{input} is correct!"
+        number_limit += 2
+      else
+        incorrect += 1
+        puts "I'm sorry, #{input} is incorrect. Don't worry, you will get a chance to try again."
+        @redos.push problem
+      end
     else
-      incorrect += 1
-      puts "I'm sorry, the correct answer was #{answer}."
+      puts "I'm sorry, you ran out of time before you answered. I'll just add this to the list of redos!"
+      @redos.push problem
     end
   end
+  puts "You got #{correct} correct, and #{incorrect} incorrect."
+  unless (correct + incorrect) > 0
+    final_grade = 0
+  else
+    final_grade = ((correct/(correct+incorrect).to_f)*100).round
+  end
+  puts "Your final grade is #{final_grade}%"
+  file.puts("#{right_now.strftime(fmt='%T')}  #{player_name.downcase} got #{correct} out of #{correct + incorrect}, for a grade of #{final_grade}%.")
+  file.close
+  puts "Now for the redos!"
+  while @redos.length > 0
+    problem = @redos.pop
+    problem.prompt
+    input = gets.chomp.to_i
+    if input == problem.answer
+      puts "#{input} is correct!"
+    else
+      puts "I'm sorry, #{input} is incorrect. Don't worry, you will get a chance to try again."
+      @redos.push problem
+    end
+  end
+  puts "Good job, you've successfully completed your #{incorrect} redos!"
 end
