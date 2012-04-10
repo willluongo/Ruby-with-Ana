f5935fb7bb52b64b0d128a308ccc93b4ad526eb4
Will Luongo <me@willluongo.com>
2012-04-09 20:32:10 -0400

    Update program to track by week
    Refactored for legibility
    Improved some formatting and terminology
    	modified:   TimedMathGame.rb

--- TimedMathGame.rb
+++ TimedMathGame.rb
@@ -1,5 +1,7 @@
+#Reqire date is using code already written for us to help us with the time and date.
 require 'date'
 
+#when the timer is put on for a certain amount of time it keeps track of that time and when it goes off it is reset
 class Timer
   def initialize
     @start_time = DateTime.now
@@ -12,6 +14,7 @@ class Timer
   end
 end
 
+#Operators "plus", "minus", "divided by", and "times" are chosen at random 
 class Problem
   def initialize(max_value)
     @type = rand(4)
@@ -31,6 +34,7 @@ class Problem
     end
   end
   
+  #random numbers are used with the operator to form a problem for the player to solve
   def prompt
     unless @type == 2
       puts "What is #{@num_1} #{@operator} #{@num_2}?"
@@ -56,23 +60,26 @@ class Problem
 end
     
 if __FILE__ == $0
+	
+	# Set abort to true so that errors in the thread do not fail silently
   Thread.abort_on_exception = true
   puts "Greetings traveler, what is your name?"
   player_name = gets.chomp
   
   right_now = DateTime.now
   @redos = []
-  this_day = right_now.strftime(fmt='%Y%m%d')
-  player_file = "./#{player_name.downcase}-#{this_day}.txt"
+  this_week = right_now.strftime(fmt='%U')
+  player_file = "./#{player_name.downcase}-#{this_week}.txt"
   
-        
+        #If the person exists just add their work in their file, if it's a different person make a new file
   if File.exists?(player_file)
     file = File.open(player_file, "a+")
   else
     file = File.new(player_file, "w+")
   end
-  puts "You've practiced #{file.readlines.length} times today."
+  puts "You've practiced #{file.readlines.length} times this week."
   
+  #All players start out with 0 incorrect and correct, number limit 11, a new timer, and no interrupts until timer goes off and player hits enter.the reason the limit is 11 is because the 0 will make it so if I put 10 there would only be 9 options.
   puts "How long in seconds are you going to practice math today?"
   time_limit = gets.chomp.to_f
   correct = 0
@@ -81,6 +88,7 @@ if __FILE__ == $0
   timer = Timer.new
   interrupt = false
   
+  #until the players time is up it will use sleep so our computer does not over work and become slow
   Thread.new {
     while true
       if timer.passed >= time_limit
@@ -92,6 +100,7 @@ if __FILE__ == $0
     end
   }
   
+  #Until the players time is up the player will be told how much time he/she has left. Under different circumstances different messages will be used. "You have____seconds left...___is incorect...ran out of time before you answered...you got __correct and __incorrect...Successfully completed redos...whatever else"
   until interrupt
     puts "\n\n\nYou have #{(time_limit - timer.passed).round} seconds left."
     problem = Problem.new(number_limit)
@@ -119,7 +128,9 @@ if __FILE__ == $0
     final_grade = ((correct/(correct+incorrect).to_f)*100).round
   end
   puts "Your final grade is #{final_grade}%"
-  file.puts("#{right_now.strftime(fmt='%T')}  #{player_name.downcase} got #{correct} out of #{correct + incorrect}, for a grade of #{final_grade}%.")
+  if (correct + incorrect) > 14
+  	file.puts("#{right_now.strftime(fmt='%A %T')} In #{time_limit} seconds, #{player_name.capitalize} got #{correct} out of #{correct + incorrect}, for a grade of #{final_grade}%.")
+	end
   file.close
   puts "Now for the redos!"
   while @redos.length > 0
